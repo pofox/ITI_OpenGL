1b1f7e04971e499f2f3e3791f2c14d6ba765981b
pofox <66141963+pofox@users.noreply.github.com>
2024-11-25 08:46:28 +0200

    Add files via upload

---
+++ VS.glsl
@@ -0,0 +1,20 @@
+#version 330
+
+layout (location = 0) in vec4 vertex_position;
+layout (location = 1) in vec4 vertex_color;
+
+uniform float theta;
+
+out vec4 frag_color;
+void main()
+{
+	gl_Position = vertex_position;
+
+	//float x = vertex_position.x;
+	//float y = 0.5*sin((x+theta) * 3.14 * 36)*sin((x+theta)*3.14*3);
+	
+	//gl_Position = vec4(x,y,vertex_position.z,1);
+
+	//frag_color = vertex_position;
+	frag_color = vertex_color;
+}
